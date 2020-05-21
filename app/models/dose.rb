fca9e2b673a32467f2ea2ce649a7e9e15570167a
Anthony Christen <anthony1998@bluewin.ch>
2020-05-21 17:50:05 +0200

    sauvegarde jeudi

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id, message: 'Couple already exists' }
 end
